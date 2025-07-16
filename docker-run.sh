4d3846903e2420f66853f59f83399c2d407a19f4
Andreea Popescu <andreea.popescu@reef.pl>
2025-07-16 13:00:15 +0100

    fix missing wallet

--- docker-run.sh
+++ docker-run.sh
@@ -11,5 +11,6 @@ docker build -t "$PYLON_DOCKER_IMAGE_NAME" .
 docker run --rm \
   --env-file .env \
   -v "$PYLON_DB_DIR:/app/db/" \
+  -v "$HOME/.bittensor:/root/.bittensor" \
   -p "$DOCKER_HOST_PORT:8000" \
   "$@" $PYLON_DOCKER_IMAGE_NAME
