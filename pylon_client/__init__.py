e9cff7142f8c53838c7b16ef38e35f5fe8725b51
Andreea Popescu <andreea.popescu@reef.pl>
2025-08-15 13:19:18 +0100

    client v0.0.6

--- pylon_client/__init__.py
+++ pylon_client/__init__.py
@@ -9,7 +9,7 @@ from pylon_common.models import (
 from .async_client import AsyncPylonClient
 from .client import PylonClient
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 __all__ = [
     "PylonClient",
